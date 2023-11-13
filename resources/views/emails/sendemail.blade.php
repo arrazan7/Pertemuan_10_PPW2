1d1b96e5fb220457c1563dc2c689c3eaeb93d193
Fayyadh Arrazan Miftakhul <113814875+arrazan7@users.noreply.github.com>
2023-11-13 12:59:38 +0700

    asal aja

--- resources/views/emails/sendemail.blade.php
+++ resources/views/emails/sendemail.blade.php
@@ -4,7 +4,7 @@
     <title>Praktikum Pemrograman Web</title>
 </head>
 <body>
-    <h1>Selamat Datang</h1>
+    <h1>Selamat Datang Selamat Datang Selamat Datang Selamat Datang</h1>
     <p>Halo Halo <strong>{{ $data['name'] }}</strong>!!!, anda telah berhasil mendaftarkan email anda di website kami. </p>
     <p>Terima kasih</p>
 </body>
